a78c9c6ae3b9c4afb4fe421aa1f0a32c97508fb4
sage <laymonage@gmail.com>
2019-10-29 10:09:38 +0700

    Add DetailFragment

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/sage/santun/ui/main/DetailFragment.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/sage/santun/ui/main/DetailFragment.kt
@@ -6,15 +6,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.databinding.DataBindingUtil
-import id.ac.ui.cs.mobileprogramming.sage.santun.databinding.MainFragmentBinding
-import id.ac.ui.cs.mobileprogramming.sage.santun.R
 
-class MainFragment : Fragment() {
+import id.ac.ui.cs.mobileprogramming.sage.santun.R
+import id.ac.ui.cs.mobileprogramming.sage.santun.databinding.DetailFragmentBinding
+
+class DetailFragment : Fragment() {
 
     companion object {
-        fun newInstance() = MainFragment()
+        fun newInstance() = DetailFragment()
     }
 
     private lateinit var viewModel: MainViewModel
@@ -22,23 +22,14 @@ class MainFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        val binding: MainFragmentBinding = DataBindingUtil.inflate(
-                inflater, R.layout.main_fragment, container, false
+    ): View? {
+
+        val binding: DetailFragmentBinding = DataBindingUtil.inflate(
+            inflater, R.layout.detail_fragment, container, false
         )
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
         binding.viewModel = viewModel
         return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        view.findViewById<Button>(R.id.detail_button).setOnClickListener {
-            fragmentManager!!.beginTransaction()
-                .replace(R.id.container, DetailFragment.newInstance())
-                .addToBackStack(null)
-                .commit()
-        }
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
