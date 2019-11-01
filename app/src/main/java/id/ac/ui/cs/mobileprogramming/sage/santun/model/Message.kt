7fee51ec62156e3dffa2bb4d20344378c922cda6
sage <laymonage@gmail.com>
2019-11-01 14:36:40 +0700

    Add Message model

---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/sage/santun/model/Message.kt
@@ -0,0 +1,12 @@
+package id.ac.ui.cs.mobileprogramming.sage.santun.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "message_table")
+data class Message(
+    @PrimaryKey(autoGenerate = true) val id: Long,
+    val sender: String,
+    val receiver: String,
+    val message: String
+)
