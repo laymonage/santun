314f37f309bbfa24189227d2cd50cd7db841fbb2
sage <laymonage@gmail.com>
2019-11-01 14:00:22 +0700

    Bind ComposeFragment to ComposeViewModel

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/sage/santun/ui/compose/ComposeViewModel.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/sage/santun/ui/compose/ComposeViewModel.kt
@@ -1,7 +1,10 @@
 package id.ac.ui.cs.mobileprogramming.sage.santun.ui.compose
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ComposeViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    val from = MutableLiveData<String>()
+    val to = MutableLiveData<String>()
+    val message = MutableLiveData<String>()
 }
