d3cf358718f9307b1ef2abc40fccc54530c920aa
sage <laymonage@gmail.com>
2019-11-02 08:43:28 +0700

    Add MessageViewModel

---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/sage/santun/model/MessageViewModel.kt
@@ -0,0 +1,22 @@
+package id.ac.ui.cs.mobileprogramming.sage.santun.model
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class MessageViewModel(application: Application) : AndroidViewModel(application) {
+    private val repository: MessageRepository
+    val allMessages: LiveData<List<Message>>
+
+    init {
+        val messageDao = MessageRoomDatabase.getDatabase(application).messageDao()
+        repository = MessageRepository(messageDao)
+        allMessages = repository.allMessages
+    }
+
+    fun insert(message: Message) = viewModelScope.launch {
+        repository.insert(message)
+    }
+}
